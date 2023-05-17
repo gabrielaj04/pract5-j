c8ad31073c42021b1b2fb49eb724bec4de96a450
gabrielaj04 <00135723@uca.edu.sv>
2023-05-17 15:08:28 -0600

    feat: modcod

--- pract5.cpp
+++ pract5.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    int num1 = 8, num2 = 4;
+    int num1 = 12, num2 = 5;
     cout << "el resultado de la resta es: " << num1 - num2;
-    
+
 
     return 0;
 }
