49f8fc92e849cd397d7975c3861f88738f2f1deb
DiegoCarrillo11 <00076521@uca.edu.sv>
2023-05-17 14:28:36 -0600

    feat: codigo

--- pract5.cpp
+++ pract5.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-int main(){
-int num1 = 8, num2 = 4;
-cout << "el resultado de la resta es: "<< num1 - num2;
+int main()
+{
+    int num1 = 8, num2 = 4;
+    cout << "el resultado de la resta es: " << num1 - num2;
 
     return 0;
 }
